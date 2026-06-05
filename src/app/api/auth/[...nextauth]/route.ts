8c5851fe154dfa4638483f0fc546ac6ee750566c
Arunkumar <arunkumar283512@gmail.com>
2026-06-05 11:04:42 +0530

    api key updated

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,3 @@
-import { NextRequest, NextResponse } from "next/server";
+import { handlers } from "@/auth";
 
-export const dynamic = "force-dynamic";
-
-export async function GET(request: NextRequest, context: any) {
-  const authModule = await import("@/auth");
-  if (!authModule || !authModule.handlers) {
-    return new NextResponse("Server initializing...", { status: 503 });
-  }
-  return authModule.handlers.GET(request, context);
-}
-
-export async function POST(request: NextRequest, context: any) {
-  const authModule = await import("@/auth");
-  if (!authModule || !authModule.handlers) {
-    return new NextResponse("Server initializing...", { status: 503 });
-  }
-  return authModule.handlers.POST(request, context);
-}
+export const { GET, POST } = handlers;
